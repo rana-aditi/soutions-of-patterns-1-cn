852a3daf5d6edcf25f7279f0102a02ea7c94c5de
rana-aditi <aditi0080.be21@chitkara.edu.in>
2022-03-24 15:03:02 +0530

    First comment added

---
+++ Question-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>  
+#include<math.h>  
+using namespace std;  
+int main()  
+{  
+    int x= -9;  
+    std::cout << "Value of x is :" <<x<< std::endl;  
+    cout<<"Absolute value of x is : "<<abs(x);  
+  
+      
+   return 0;  
+}  
+//oops
+
